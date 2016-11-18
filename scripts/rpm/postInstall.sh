9e58c52981994b3240285f857e8ee693d650d2c5
jprieto <jprieto@hotelbeds.com>
2016-11-18 12:29:33 +0100

    postInstall

--- scripts/rpm/postInstall.sh
+++ scripts/rpm/postInstall.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 chown daemon-demo:daemon-demo /opt/local/daemon-demo/log
+ln -s /etc/init.d/daemon-demo /etc/rc2.d/S90daemon-demo
+ln -s /etc/init.d/daemon-demo /etc/rc3.d/S90daemon-demo
+ln -s /etc/init.d/daemon-demo /etc/rc4.d/S90daemon-demo
+ln -s /etc/init.d/daemon-demo /etc/rc5.d/S90daemon-demo
