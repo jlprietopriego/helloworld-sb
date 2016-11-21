4ab89cfa9b85bb6df282ed8b94fce856edc8fd1a
jprieto <jprieto@hotelbeds.com>
2016-11-21 13:40:46 +0100

    HBG Group

--- src/main/java/me/ccampo/daemondemo/Application.java
+++ src/main/java/me/ccampo/daemondemo/Application.java
@@ -17,7 +17,7 @@ public class Application {
 
         @RequestMapping
         public String helloWorld() {
-            return "Hello world!";
+            return "Hotelbeds Group says Hello World!";
         }
     }
 }
