7518a0318a43b9709025f11f9baa2d3efbd83169
jprieto <jprieto@hotelbeds.com>
2016-11-21 17:43:16 +0100

    0.0.2 again

--- src/main/java/me/ccampo/daemondemo/Application.java
+++ src/main/java/me/ccampo/daemondemo/Application.java
@@ -17,7 +17,7 @@ public class Application {
 
         @RequestMapping
         public String helloWorld() {
-            return "Hello World!";
+            return "Hotelbeds Group says Hello World!";
         }
     }
 }
